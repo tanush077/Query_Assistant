56f372f341782f973b9be183f23fa3163913535e
Abhinav Sharma <113504774+abhinavs1920@users.noreply.github.com>
2023-10-28 18:18:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,38 @@ def submit_query():
     except Exception as e:
         messagebox.showerror("Error", str(e))
 
+def generate_query(user_input):
+
+    base_query = "SELECT * FROM users WHERE"
+
+    filters = user_input.split(",")
+
+    conditions = []
+    for filter_part in filters:
+        key, value = filter_part.split(":")  # Assuming user input is in 'key: value' format
+        key = key.strip()
+        value = value.strip()
+
+        if key == 'name':
+            conditions.append(f" name = '{value}'")
+        elif key == 'age':
+            conditions.append(f" age = {value}")
+        elif key == 'city':
+            conditions.append(f" city = '{value}'")
+
+    if conditions:
+        query = f"{base_query} {' AND '.join(conditions)}"
+        return query
+    else:
+        return "Invalid input. Please provide valid filters."
+
+# Example user input
+user_text_input = "name: John, age: 30, city: New York"
+
+query = generate_query(user_text_input)
+print(query)
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("SQL Query App")
